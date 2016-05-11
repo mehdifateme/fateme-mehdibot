4245a545e29493f14d88ddf5a1b301674b889dbe
mehdifateme <Moqadasomehdi@gmail.com>
2016-05-11 18:31:57 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,30 +226,25 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = { 188172284,227323836},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[Mehdi-FatemeBOT V2 - Open Source
+An advance Administration bot based on SEEDTEAM/TeleSeed
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/MehdiFateme/Fateme-Mehdibot
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+Mehdi OKTAY
+Fateme OKTAY
 
 Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
+ali OKTAY
+LORD OKTAY
+SEZAR OKTAY
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+Persian: @KosMmber
 ]],
     help_text_realm = [[
 Realm Commands:
